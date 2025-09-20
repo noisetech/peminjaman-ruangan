26e01c4a6c78b5bb4a4713451d82848cd22678d7
noisetech <box92143@gmail.com>
2025-09-20 20:56:00 +0700

    finish template tinggal db

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +22,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::prefix('dashboard')->group(function(){
+    Route::get('/', [DashboardController::class, 'index']);
+});
